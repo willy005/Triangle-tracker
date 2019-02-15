4800cb6b41f86d148953d881bf8f2ee7f4a6d806
nillyw31@gmail.com <nillyw31@gmail.com>
2019-02-15 11:03:15 +0300

    Add all changes made

--- js/lib.js
+++ js/lib.js
@@ -1,17 +1,17 @@
- // function ShowResults(){
- //   var hypotenuse = parseFloat(prompt("Enter hypotenuse side"));
- //   var opposite = parseFloat(prompt("Enter opposite side"));
- //   var adjacent = parseFloat(prompt("Enter adjacent side"));
+ function ShowResults(){
+   var hypotenuse = parseFloat(prompt("Enter hypotenuse side"));
+   var opposite = parseFloat(prompt("Enter opposite side"));
+   var adjacent = parseFloat(prompt("Enter adjacent side"));
 
-function ShowResults(){
-  var sides=[]
-  sides.push(document.getElementById('hypotenuse').value);
-  sides.push(document.getElementById('opposite').value);
-  sides.push(document.getElementById('adjacent').value);
-
-  var hypotenuse = sides[0]
-  var opposite = sides[1]
-  var adjacent = sides[2]
+// function ShowResults(){
+//   var sides=[]
+//   sides.push(document.getElementById('hypotenuse').value);
+//   sides.push(document.getElementById('opposite').value);
+//   sides.push(document.getElementById('adjacent').value);
+//
+//   var hypotenuse = sides[0]
+//   var opposite = sides[1]
+//   var adjacent = sides[2]
 
 if((hypotenuse + opposite) <= adjacent || (hypotenuse + adjacent) <= opposite || (opposite + adjacent) <= hypotenuse) {
   output.innerHTML=("No Triangle")

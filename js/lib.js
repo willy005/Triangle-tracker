5eed4108889b5b4724f386280e2a8779c2cb3d8b
nillyw31@gmail.com <nillyw31@gmail.com>
2019-02-15 11:26:34 +0300

    Add changes made

--- js/lib.js
+++ js/lib.js
@@ -1,29 +1,23 @@
- function ShowResults(){
-   var hypotenuse = parseFloat(prompt("Enter hypotenuse side"));
-   var opposite = parseFloat(prompt("Enter opposite side"));
-   var adjacent = parseFloat(prompt("Enter adjacent side"));
+function ShowResults(){
+  var sides=[]
+  sides.push(document.getElementById('hypotenuse').value);
+  sides.push(document.getElementById('opposite').value);
+  sides.push(document.getElementById('adjacent').value);
 
-// function ShowResults(){
-//   var sides=[]
-//   sides.push(document.getElementById('hypotenuse').value);
-//   sides.push(document.getElementById('opposite').value);
-//   sides.push(document.getElementById('adjacent').value);
-//
-//   var hypotenuse = sides[0]
-//   var opposite = sides[1]
-//   var adjacent = sides[2]
+  var hypotenuse = sides[0]
+  var opposite = sides[1]
+  var adjacent = sides[2]
 
 if((hypotenuse + opposite) <= adjacent || (hypotenuse + adjacent) <= opposite || (opposite + adjacent) <= hypotenuse) {
   output.innerHTML=("No Triangle")
 }
-
 else if(hypotenuse === opposite && hypotenuse === adjacent && opposite === adjacent){
 output.innerHTML=("Equilateral Triangle")
 }
 else if(hypotenuse === opposite || hypotenuse === adjacent || opposite === adjacent){
 output.innerHTML=("Isoceles Triangle")
 }
-else if (hypotenuse!==opposite && hypotenuse!==adjacent && opposite!==adjacent){
+else if(hypotenuse!==opposite && hypotenuse!==adjacent && opposite!==adjacent){
 alert("Scalene");
 }
 }

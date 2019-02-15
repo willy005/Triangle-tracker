be4d37c56b719fe28613a229d91d135f998b26f4
nillyw31@gmail.com <nillyw31@gmail.com>
2019-02-15 16:54:22 +0300

    Add changes made about parse Int

--- js/lib.js
+++ js/lib.js
@@ -4,20 +4,22 @@ function ShowResults(){
   sides.push(document.getElementById('opposite').value);
   sides.push(document.getElementById('adjacent').value);
 
-  var hypotenuse = sides[0]
-  var opposite = sides[1]
-  var adjacent = sides[2]
+  var hypotenuse = parseInt(sides[0]);
+  var opposite = parseInt(sides[1]);
+  var adjacent = parseInt(sides[2]);
 
 if((hypotenuse + opposite) <= adjacent || (hypotenuse + adjacent) <= opposite || (opposite + adjacent) <= hypotenuse) {
   output.innerHTML=("Not a Definite Triangle")
 }
+
 else if(hypotenuse === opposite && hypotenuse === adjacent && opposite === adjacent){
 output.innerHTML=("Equilateral Triangle")
-.
 }
+
 else if(hypotenuse === opposite || hypotenuse === adjacent || opposite === adjacent){
 output.innerHTML=("Isoceles Triangle")
 }
+
 else if(hypotenuse!==opposite && hypotenuse!==adjacent && opposite!==adjacent){
 alert("Scalene");
 }
